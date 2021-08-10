55675726b37e2855e6016ba3baa0f05f0c973d6a
Tomas Vidal <tomi_vidal_99@hotmail.com>
2021-08-10 15:23:40 -0300

    added the main functionallity

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+// modify your existing createWindow() function
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
