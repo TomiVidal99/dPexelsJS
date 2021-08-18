3e5afb23f73e0235b1ea33bce9ac9b17f6870073
Tomas Vidal <tomi_vidal_99@hotmail.com>
2021-08-17 23:23:51 -0300

    added downlaod functions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dPexelsJS
 Desktop app made with electron to download bulk quantity of images from pexels API.
+### TODO
+* - [ ] on preview hover make the picture bigger.
+* - [ ] add progress bar.
+* - [ ] fix the slider, make it better and add a input to the number, not just slider.
+* - [ ] add select for min and max width and height.
+* - [ ] add functionallity to the sizes select.
+
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
+const { download_image } = require('./App/scripts/handle_download_image.js')
 const path = require('path')
 const isDev = process.env.APP_DEV ? (process.env.APP_DEV.trim() == "true") : false
 
@@ -25,4 +26,15 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow()
+})
+
+// handle the file download (must be handled on the main thread)
+ipcMain.on('download-image', (event, url, filepath, filename, query) => {
+    console.log('got data, will download', url, filepath, filename)
+    download_image(url, path.join(filepath, query), filename).then( (res) => {
+        event.reply('image-downloaded', 'downloaded')
+    }).catch( (err) => {
+        if (err) throw err
+        event.reply('image-downloaded', 'error')
+    })
 })
